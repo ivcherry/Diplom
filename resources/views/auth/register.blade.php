95cccf597c56c15283be8824a18f6c0af763803a
Angainor17 <angainor17@gmail.com>
2019-05-17 12:57:14 +0300

    Revert "Исправил ошибки в адаптивности страниц."
    This reverts commit 24371012

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,58 +3,58 @@
 @section('content')
     <div class="container">
         <div class="row auth">
-            <div class="panel panel-default panel-auth">
-                <div class="panel-heading" style="margin: 20px 0;"><h4>Регистрация</h4></div>
+            <div class="col-md-8 col-md-offset-2 panel-auth">
+                <div class="panel panel-default" style="margin: 10px 0; padding-left: 50px;">
+                    <div class="panel-heading" style="margin: 20px 0;"><h4>Регистрация</h4></div>
 
-                <div class="panel-body reg">
-                    <form class="form-horizontal" method="POST" action="/auth/register">
-                        {{ csrf_field() }}
+                    <div class="panel-body reg">
+                        <form class="form-horizontal" method="POST" action="/auth/register">
+                            {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('fullName') ? ' has-error' : '' }}">
-                            <label for="fullName">Имя</label>
-                            <div>
-                                <input id="fullName" type="text" class="form-control form-provide" name="fullName"
-                                       value="{{ old('fullName') }}" required autofocus>
-                                @if ($errors->has('fullName'))
-                                    <span class="help-block"><strong>{{ $errors->first('fullName') }}</strong></span>
-                                @endif
+                            <div class="form-group{{ $errors->has('fullName') ? ' has-error' : '' }}">
+                                <label for="fullName" class="col-md-4 control-label">Имя</label>
+                                <div class="col-md-6">
+                                    <input id="fullName" type="text" class="form-control" name="fullName" value="{{ old('fullName') }}" required autofocus>
+                                    @if ($errors->has('fullName'))
+                                        <span class="help-block"><strong>{{ $errors->first('fullName') }}</strong></span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email">E-Mail</label>
-                            <div>
-                                <input id="email" type="email" class="form-control form-provide" name="email"
-                                       value="{{ old('email') }}" required>
-                                @if ($errors->has('email'))
-                                    <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
-                                @endif
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                    @if ($errors->has('email'))
+                                        <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password">Пароль</label>
-                            <div>
-                                <input id="password" type="password" class="form-control form-provide" name="password" required>
-                                @if ($errors->has('password'))
-                                    <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
-                                @endif
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Пароль</label>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+                                    @if ($errors->has('password'))
+                                        <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm">Подтвердение пароля</label>
-                            <div>
-                                <input id="password-confirm" type="password" class="form-control form-provide" name="password_confirmation" required>
+                            <div class="form-group">
+                                <label for="password-confirm" class="col-md-4 control-label">Подтвердение пароля</label>
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div>
-                                <button type="submit" class="btn btn-primary btn-md">Зарегистрироваться</button>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary btn-md">Зарегистрироваться</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/client/tuning/index.blade.php
+++ resources/views/client/tuning/index.blade.php
@@ -5,7 +5,6 @@
     <div class="order-form" style="font-size: 14px;">
         <form id="installForm" action="/thanksForOrdering"  method="POST">
             {{ csrf_field() }}
-            <h3>Настройка техники</h3>
             <div class="form-group">
                 <span>ФИО</span>
                 <div>
@@ -35,13 +34,12 @@
                 </div>
             </div>
             <div class="form-group">
-                <span>Укажите, то нуждается в ремонте</span>
+                <span>Техника, которую необходимо починить:</span>
                 <div>
-                  <select class="form-control" name="installingPO" size="1" style="width: 50%;">
-                      <option selected disabled>Выбрать</option>
-                      <option value="1">Комплектующие ПК</option>
-                      <option value="3">Windows</option>
-                      <option value="3">Другие проблемы</option>
+                  <select name="installingPO" size="1">
+                      <option value="1">Монитор</option>
+                      <option value="2">Колонки</option>
+                      <option value="3">omae wa mou shindeiru</option>
                   </select>
                 </div>
             </div>
@@ -49,14 +47,13 @@
             <div class="form-group">
                 <span>Описание проблемы</span>
                 <div>
-                    {{--<input name="summary" type='text' id="summary" class="form-control"--}}
-                           {{--style="width: 100%; height: 100px">--}}
-                    <textarea name="summary" id="summary" cols="30" rows="5" class="form-control" placeholder="Укажите  возникжие у Вас проблемы"></textarea>
+                    <input name="summary" type='text' id="summary" class="form-control"
+                           style="width: 100%; height: inherit">
                 </div>
             </div>
 
             <div class="text-center">
-                <button class="btn btn-primary btn-md" type="submit" id="installBtn">Заказать услугу</button>
+                <button class="btn btn-done" type="submit" id="installBtn">Заказать услугу</button>
             </div>
 
         </form>
